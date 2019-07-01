28d62a5fcf591f6466b087bb49ee04479f93eca4
Charles Fyfe <cfyfe@cray.com>
2019-07-01 08:49:43 -0500

    added demographic data

--- states.py
+++ states.py
@@ -4,30 +4,54 @@ import helpers
 
 def main():
 
-
-    return helpers.scatter(
+    helpers.scatter(
         loadfile="data/states.json",
         xkey = "population",
-        ykey = "rain_inches",
+        ykey = "pct_white",
+        title="Larger States Are Generally More Diverse",
         xlabel="Population",
+        ylabel="Non-Hispanic White Population (%)",
+        ylim=[0, 100],
         log="x",
         xlim=[1e5, 1e8],
-
+        savefile="example-plot-log.png"
     )
 
+    helpers.scatter(
+        loadfile="data/states.json",
+        xkey = "population",
+        ykey = "pct_white",
+        title="Are Larger States Generally More Diverse?",
+        xlabel="Population (millions)",
+        ylabel="Non-Hispanic White Population (%)",
+        ylim=[0, 100],
+        log="",
+        trend=False,
+        xticks=[0, 1e7, 2e7, 3e7, 4e7],
+        xticklabels=[0, 10, 20, 30, 40],
+        savefile="example-plot-lin.png"
+    )
+
+    return
 
 
     data = helpers.load_json("data/states.json")
 
     new = {}
 
-    for line in helpers.read_lines("data/states2.txt"):
-        name = " ".join(line.split()[:-3])
-        new[name] = float(line.split()[-3])
+    for line in helpers.read_lines("states.in"):
+
+        names = line.split()[:-11]
+        # Listed twice...
+        name = " ".join(names[:len(names)//2])
+        name = name.split("(")[0].strip()
+        new[name] = float(line.split()[-4].strip("%"))
+
+    [ print(k, ":", v) for k, v in new.items() ]
 
 
     for state in data:
-        state["rain_inches"] = new[state["full_name"]]
+        state["pct_white"] = new[state["full_name"]]
 
     helpers.dump_json(data, "data/states.json")
 
