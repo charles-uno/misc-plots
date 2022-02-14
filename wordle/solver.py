4e8437f16454a024ba73e7d41a418f7a9fe59df3
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2022-02-13 21:25:27 -0600

    dump to file

--- wordle/solver.py
+++ wordle/solver.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
+import os
 import random
 import sys
 
-OPENING_GUESS = 'crane'
-
-N = 1000
+N = 50
 N_COLS = 10
+OUT_DIR = 'out'
 
 LENGTH = 5
 MAX_GUESSES = 6
 
 GREEN = '\033[92m'
+MAGENTA = '\033[35m'
 RESET = '\033[0m'
 YELLOW = '\033[93m'
+RED = '\033[31m'
 BLACK = ''
 
 ALPHABET = [chr(i) for i in range(ord('a'), ord('z')+1)]
@@ -26,72 +28,94 @@ with open('solutions.txt', 'r') as handle:
 
 
 def main():
-    histogram = {i: 0 for i in range(MAX_GUESSES+1)}
+    openers = get_openers()
     for _ in range(N):
-        result = Result()
-        histogram[result.turns] += 1
-        print_across(result)
-    print_stats(histogram)
+        opener = random.choice(openers) if openers else None
+        Result(opener).dump()
+    print_stats()
 
 
-def print_stats(histogram):
+def get_openers():
+    openers = [x.lower() for x in sys.argv[1:]]
+    assert all(x.isalpha() for x in openers)
+    assert all(len(x) == LENGTH for x in openers)
+    return openers
+
+
+def print_stats():
+    filenames = os.listdir(OUT_DIR)
+    print()
+    [print_stats_helper(os.path.join(OUT_DIR, f)) for f in sorted(filenames)]
+
+
+def print_stats_helper(filename):
+    with open(filename, 'r') as handle:
+        vals = [int(x.strip()) for x in handle]
+    histogram = {i: 0 for i in range(MAX_GUESSES+1)}
+    for v in vals:
+        histogram[v] += 1
     whiffs = histogram[0]
     total = sum(histogram.values())
     num = sum(n*histogram[n] for n in range(1, MAX_GUESSES+1))
     denom = sum(histogram[n] for n in range(1, MAX_GUESSES+1))
     avg = "%.2f" % (num/denom)
-    print()
-    [print(i, histogram[i]) for i in range(MAX_GUESSES+1)]
-    print("success rate:", fmt(total - whiffs, total))
-    print("average guesses:", avg)
+    pad = ' '*5
+    print(filename, pad, avg, pad, fmt(total - whiffs, total))
 
 
 def fmt(num, denom):
     p = (num*100./denom)
     dp = 100./(denom)**0.5
-    return "%.0f%% ± %.0f%%" % (p, dp)
+    return "%.1f%% ± %.1f%%" % (p, dp)
 
 
 PRINT_CACHE = []
 
-
-def print_across(result):
-    global PRINT_CACHE
-    PRINT_CACHE.append(result)
-    if len(PRINT_CACHE) == N_COLS:
-        padding = ' '*2
-        for i in range(MAX_GUESSES):
-            print('   ', end='')
-            for result in PRINT_CACHE:
-                line = result[i] if len(result) > i else ' '*LENGTH
-                print(padding, line, end='')
-            print()
-        print()
-        PRINT_CACHE = []
-    return
-
-
 class Result(list):
 
-    def __init__(self):
-        self.solution = random.choice(SOLUTIONS)
+    def __init__(self, opener):
         self.turns = 0
+        self.opener = opener
+        solution = random.choice(SOLUTIONS)
         lines = []
         allowed = SOLUTIONS
         constraints = Constraints()
         for i in range(1, MAX_GUESSES+1):
             # More efficient to shuffle the list then take the first match?
             allowed = [x for x in allowed if constraints.check(x)]
-            guess = random.choice(allowed)
-            if OPENING_GUESS and not lines:
-                guess = OPENING_GUESS
-            clues = Clues(guess, self.solution)
+            if opener and not lines:
+                guess = opener
+            else:
+                guess = random.choice(allowed)
+            clues = Clues(guess, solution)
             lines.append(clues)
             constraints.update(clues)
-            if guess == self.solution:
+            if guess == solution:
                 self.turns = i
                 break
         return list.__init__(self, lines)
+
+    def dump(self):
+        os.makedirs(OUT_DIR, exist_ok=True)
+        filename = '%s.csv' % self.opener if self.opener else 'random.csv'
+        with open(os.path.join(OUT_DIR, filename), 'a') as handle:
+            handle.write('%d\n' % self.turns)
+        self.print_across()
+
+    def print_across(self):
+        global PRINT_CACHE
+        PRINT_CACHE.append(self)
+        if len(PRINT_CACHE) == N_COLS:
+            print()
+            padding = ' '*2
+            for i in range(MAX_GUESSES):
+                print('   ', end='')
+                for result in PRINT_CACHE:
+                    line = result[i] if len(result) > i else ' '*LENGTH
+                    print(padding, line, end='')
+                print()
+            PRINT_CACHE = []
+        return
 
 
 class Constraints(list):
@@ -100,11 +124,11 @@ class Constraints(list):
         elts = [Constraint(c) for c in ALPHABET]
         return list.__init__(self, elts)
 
-    def update(self, clues):
-        [c.update(clues) for c in self]
-
     def check(self, guess):
         return all(c.check(guess) for c in self)
+
+    def update(self, clues):
+        [c.update(clues) for c in self]
 
 
 class Constraint(object):
@@ -153,6 +177,9 @@ class Clues(list):
 
     def __str__(self):
         return "".join(str(x) for x in self)
+
+    def text(self):
+        return "".join(x.letter for x in self)
 
 
 class Clue(object):
