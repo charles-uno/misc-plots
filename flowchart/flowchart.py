2772916ea95253b2644e5be0a613edf42e873563
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-01-09 12:51:15 -0600

    better layout engine

--- flowchart/flowchart.py
+++ flowchart/flowchart.py
@@ -8,38 +8,63 @@ from enum import Enum
 import pygraphviz as pgv
 
 ABORTION_RATE = "abortion\nrate"
-CAR_OWNERSHIP = "car\nownership"
 CIVIC_TRUST = "civic\ntrust"
+CLIMATE_CHANGE = "climate\nchange"
+CONCENTRATED_POVERTY = "concentrated\npoverty"
 DOMESTIC_VIOLENCE = "domestic\nviolence"
 HOUSING_AFFORDABILITY = "housing\naffordability"
 HOUSING_SUPPLY = "housing\nsupply"
 HOMELESSNESS = "homelessness"
+IMMIGRATION = "immigration"
 MALE_LONELINESS = "male\nloneliness"
 MASS_INCARCERATION = "mass\nincarceration"
 MASS_SHOOTINGS = "mass\nshootings"
 MASS_TRANSIT = "mass\ntransit"
+MILITARIZED_POLICE = "militarized\npolice"
+OIL_WARS = "oil\nwars"
+RACISM = "racism"
+REGIONAL_INSTABILITY = "regional\ninstability"
+RESTRICTIVE_ZONING = "restrictive\nzoning"
 SEX_EDUCATION = "sex\neducation"
 SUBSTANCE_ADDICTION = "substance\naddiction"
-URBAN_DENSITY = "urban\ndensity"
+URBAN_SPRAWL = "urban\nsprawl"
+WIDESPREAD_CARS = "widespread\ncars"
 WIDESPREAD_GUNS = "widespread\nguns"
 
 
-
 # strict (no parallel edges), digraph, with attribute rankdir set to 'LR'
-A = pgv.AGraph(directed=True, strict=True, rankdir="LR")
+A = pgv.AGraph(directed=True, rankdir="LR")
 # add node 1 with color red
 A.add_edge(HOUSING_SUPPLY, HOUSING_AFFORDABILITY)
 A.add_edge(HOUSING_AFFORDABILITY, HOMELESSNESS)
 A.add_edge(HOMELESSNESS, MASS_TRANSIT)
 A.add_edge(HOMELESSNESS, MASS_TRANSIT)
-A.add_edge(MASS_TRANSIT, CAR_OWNERSHIP)
-A.add_edge(URBAN_DENSITY, MASS_TRANSIT)
+A.add_edge(MASS_TRANSIT, WIDESPREAD_CARS)
+A.add_edge(URBAN_SPRAWL, MASS_TRANSIT)
 A.add_edge(WIDESPREAD_GUNS, MASS_SHOOTINGS)
 A.add_edge(MASS_SHOOTINGS, CIVIC_TRUST)
 A.add_edge(CIVIC_TRUST, WIDESPREAD_GUNS)
 A.add_edge(SEX_EDUCATION, ABORTION_RATE)
 A.add_edge(SUBSTANCE_ADDICTION, DOMESTIC_VIOLENCE)
+A.add_edge(RESTRICTIVE_ZONING, HOUSING_SUPPLY)
+A.add_edge(HOMELESSNESS, SUBSTANCE_ADDICTION)
+A.add_edge(CLIMATE_CHANGE, REGIONAL_INSTABILITY)
+A.add_edge(REGIONAL_INSTABILITY, IMMIGRATION)
+A.add_edge(OIL_WARS, REGIONAL_INSTABILITY)
+A.add_edge(WIDESPREAD_CARS, CLIMATE_CHANGE)
+A.add_edge(WIDESPREAD_CARS, URBAN_SPRAWL)
+A.add_edge(RACISM, RESTRICTIVE_ZONING)
+A.add_edge(RACISM, CIVIC_TRUST)
+A.add_edge(RACISM, MASS_INCARCERATION)
+A.add_edge(RACISM, MILITARIZED_POLICE)
+A.add_edge(WIDESPREAD_CARS, OIL_WARS)
+A.add_edge(IMMIGRATION, RACISM)
+A.add_edge(WIDESPREAD_GUNS, MILITARIZED_POLICE)
+
 
 print(A.string())  # print dot file to standard output
-A.layout("dot")  # layout with dot
+# default layout uses dot, which stretches things out pretty bad
+# other options: neato, fdp, twopi
+A.layout("neato", args="-Goverlap=scale")
+
 A.draw("flowchart.png")  # write to file
